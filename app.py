5222adab567246bedf4020b1fcda2aa4d5ffb08f
Jesse Huang <jessehuang2222@gmail.com>
2019-11-26 03:12:04 +0800

    Improve structure.

--- app.py
+++ app.py
@@ -1,41 +1,36 @@
-import json
+import json, obfuscator
 from flask import Flask, request, Response
-
 
 app = Flask(__name__)
 
 DEFAULT_LANGUAGE = 'bash'
 DEFAULT_OBFUSCATE = False
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = '8787'
 
 @app.route('/', methods=['GET'], defaults={'team_id': None, 'language': None, 'return_raw': None})
 @app.route('/<team_id>', methods=['GET'], defaults={'language': None, 'return_raw': None})
 @app.route('/<team_id>/<language>', methods=['GET'], defaults={'return_raw': None})
 @app.route('/<team_id>/<language>/<return_raw>', methods=['GET'])
+
 def get_reverse_shell_code(team_id, language, return_raw):
     with open('payload.json', 'r') as f:
         payload = json.loads(f.read())
 
-    with open('team.json', 'r') as f:
-        team = json.loads(f.read())
+    with open('teams.json', 'r') as f:
+        teams = json.loads(f.read())
 
-    if team_id and team_id in team:
-        host = team[team_id]['host']
-        port = team[team_id]['port']
-    else:
-        host = DEFAULT_HOST
-        port = DEFAULT_PORT
+    if not team_id in teams:
+        team_id = 'default'
+    host = teams[team_id]['host']
+    port = teams[team_id]['port']
 
-    if not language or language not in payload:
+    if language not in payload:
         language = DEFAULT_LANGUAGE
 
-    if return_raw:
-        code = payload[language]['raw']
-    else:
-        code = payload[language]['obfuscate'] if DEFAULT_OBFUSCATE else payload[language]['raw']
-    # I don't know how to replace HOST and PORT in obfuscated code QQ
-    return Response(code.format(HOST=host, PORT=port), mimetype='text/plain')
+    code = payload[language]['raw'].format(HOST=host, PORT=port)
+    if not return_raw:
+        code = eval('obfuscator.' + payload[language][obfuscator])(code)
+
+    return Response(code, mimetype='text/plain')
 
 if __name__ == '__main__':
     # TODO: setup with argv
---
+++ obfuscator.py
@@ -0,0 +1,2 @@
+def default(payload):
+    return payload
